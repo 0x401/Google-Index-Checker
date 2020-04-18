ed386e3773e355dfbb2ef72ded6ce885638f5544
brody <brodyhe@soarinfotech.com>
2020-04-18 17:15:01 +0800

    切换到extension版本

--- background.js
+++ background.js
@@ -1,14 +1,168 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('window.html', {
-    frame: "none",
-    bounds: {
-      
-      width: 1115,
-      height: 768
-    },
-    minWidth: 1115,
-    minHeight:768,
-    maxWidth: 1115,
-    maxHeight:768,
-  });
+const googleUrl = "https://www.google.com/ncr";
+const ip = ["72.14.190.106", "176.58.101.66", "103.3.61.241", "101.207.120.16"];
+const geo = ["US", "UK", "SG", "联通"];
+let index, error, num, indexed, unIndexed, stop, googleTabId, data;
+function init() {
+  googleTabId = -1;//google搜索页标签id
+  data = [];//表格url
+  index = -1;//当前url索引
+  stop = true;//搜索是否进行
+  error = false;//搜索是否错误
+  num = 0;//表格编号
+  indexed = 0;//收录数
+  unIndexed = 0;//未收录数
+  $("#total").text(num);
+}
+init();
+//导入数据
+function importTxt() {
+  $("#file").trigger("click");
+}
+$("#file").on("change", function () {
+  let file = $(this).prop('files')[0];
+  console.log(file);
+  let reader = new FileReader();
+  reader.onload = function (e) {
+    init();
+    let list = e.target.result.split(/\n\r?/);
+    $("#list>tbody").empty();
+    $.each(list, function (i, value) {
+      if (value.trim() != "") {
+        num++;
+        let url = $("#baseUrl").val() + value.trim();
+        $("#list>tbody").append('<tr id="_' + i + '"><td>' + num + '</td><td class="url">' + value + '</td><td class="status"></td><!--<td class="page"></td>--></tr>');
+        data[i] = url;
+      }
+    });
+    $("#total").text(num);
+  };
+  if (file) {
+    reader.readAsText(file);
+  }
+
+
 });
+//导出数据
+function exportTxt() {
+  let list = $("#list>tbody>tr");
+  let r = ["URL,Indexed\n"];
+  for (var i = 0; i < list.length; i++) {
+    if (list.children("td.status")[i].innerText != "") {
+      r[i + 1] = list.children("td.url")[i].innerText + "," + list.children("td.status")[i].innerText + "\n";
+    }
+  }
+  function errorHandler() {
+    console.log('a');
+  }
+  chrome.fileSystem.chooseEntry({ type: 'saveFile', suggestedName: 'a.csv' }, function (writableFileEntry) {
+    writableFileEntry.createWriter(function (writer) {
+      writer.onerror = errorHandler;
+      writer.onwriteend = function (e) {
+        console.log('write complete');
+      };
+      writer.write(new Blob(r, { type: 'text/plain' }));
+    }, errorHandler);
+  });
+
+}
+//更新线路信息
+function updateLocation() {
+  $.getJSON('https://freegeoip.app/json/', function (data) {
+    if (ip.indexOf(data.ip) > -1) {
+      $("#location").text(geo[ip.indexOf(data.ip)]);
+    } else {
+      $("#location").text(data.country_code);
+    }
+  });
+}
+updateLocation();
+$("body").on('click', '#import', importTxt);
+$("body").on('click', '#export', exportTxt);
+$("body").on('click', '#stop', function () {
+  stop = true;
+  $("#stop").prop({ "id": "check", "value": "Check" });
+});
+$("body").on('click', '#check', function () {
+  if (!data) {
+    updateTip('请导入数据');
+    return;
+  }
+  if (googleTabId == -1) {
+    chrome.tabs.create({ url: googleUrl, active: false, selected: false }, function (tab) {
+      googleTabId = tab.id;
+      updateTip("正在打开 Google 搜索页");
+    });
+  }
+});
+//更新提示
+function updateTip(tip) {
+  $("#tip").text(tip);
+}
+//更新搜索结果
+function updateStatus(index, status) {
+  //console.log('更新结果：' + index);
+  updateTip("更新结果到表格：" + data[index] + "……");
+  let statusEle = $("#_" + index).children(".status");
+  switch (status) {
+    case -2:
+      statusEle.removeClass().addClass("status is-loading is-success has-text-centered").text("Testing");
+      break;
+    case -1:
+      statusEle.removeClass().addClass("status is-warning has-text-centered").text("ERROR");
+      break;
+    case 0:
+      statusEle.removeClass().addClass("status is-danger has-text-centered no").text("NO");
+      unIndexed++;
+      $("#unIndexed").text($(".no").length);
+      break;
+    case 1:
+      statusEle.removeClass().addClass("status is-success has-text-centered yes").text("YES");
+      indexed++;
+      $("#indexed").text($(".yes").length);
+      break;
+  }
+}
+//发送搜索请求
+function search(index) {
+  //console.log('请求搜索：' + index+':'+data[index]);
+  chrome.tabs.sendMessage(googleTabId, { q: "site:" + data[index] }, function (response) {
+    updateTip("正在搜索：" + data[index] + "……");
+    updateStatus(index, response.status);
+  });
+
+}
+//获取搜索结果
+function getSearchResult(index) {
+  //console.log('请求获取结果：' + index);
+  updateTip("正在获取结果：" + data[index] + "……");
+  chrome.tabs.sendMessage(googleTabId, { g: true }, function (response) {
+    //console.log(response);
+    updateStatus(index, response.status);
+  });
+}
+//监听标签状态
+chrome.tabs.onUpdated.addListener(function (tabId, info, tab) {
+  if (googleTabId != -1 && tabId == googleTabId && info.status == 'complete') {
+    let url = new URL(tab.url);
+    switch (url.pathname) {
+      case '/sorry':
+        updateTip('搜索失败，请完成验证');
+        break;
+      case '/search':
+        //let q = new URLSearchParams(url.search).get('q');
+        getSearchResult(index);
+        index++;
+        if (data[index]) {
+          search(index);
+        } else {
+          updateTip('处理完成');
+        };
+        break;
+      default:
+        if (index == -1) {
+          index += 1;
+          search(index);
+        }
+    }
+  }
+})
--- inject.js
+++ inject.js
@@ -1,8 +1,55 @@
 
+// let url =location.href;
+// if (url.includes("/sorry/")) {
+//   status = -1;
+// } else if(url.includes("&q=")) {
+//   let keywords = [
+//     "did not match any documents",
+//     "No results found for",
+//     "未找到符合",
+//     "找不到和您查询的",
+//     "に一致する結果は見つかりませんでした",
+//     "との一致はありません"
+//   ];
+//   let topStuff = document.querySelector("#topstuff").innerText;
+//   //判断是否包含关键词
+//   if (keywords.some(v => topStuff.includes(v))) {
+//     status = 1;
+//   }
+// }else{
+//    status =0;
+// }
+let status = -2;
+chrome.runtime.onMessage.addListener(function (request, sender,sendResponse){ 
+  //处理搜索请求
+  if(request.q){
+    //sendResponse({ msg: 'ok'});
+    sendResponse({ status: status});
+    document.querySelector('input[title="Search"]').value=request.q;
+    document.querySelector('[aria-label="Google Search"]').click();
+    
+  }
+  //处理获取搜索结果请求
+  if(request.g){
+    let keywords = [
+      "did not match any documents",
+      "No results found for",
+      "未找到符合",
+      "找不到和您查询的",
+      "に一致する結果は見つかりませんでした",
+      "との一致はありません"
+    ];
+    let topStuff = document.querySelector("#topstuff").innerText;
+    //判断是否包含关键词
+    let status;
+    if (keywords.some(v => topStuff.includes(v))) {
+      status = 0;
+    }else{
+      status = 1;
+    }
+    sendResponse({ status: status});
+  }
 
-let isIndex;
-if(document.querySelector("#search")!=undefined){
-	isIndex =(document.querySelector("#search").children.length==0)?false:true;
-}
-console.log(isIndex+":"+document.URL);
-//did not match any documents
+});
+
+
