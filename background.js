422d54b082cbd092427133dfac34ba35ecf447b5
0x400 <hbn9@qq.com>
2020-04-20 16:22:21 +0800

    最后一次搜索逻辑

--- background.js
+++ background.js
@@ -1,17 +1,20 @@
 const googleUrl = "https://www.google.com/ncr";
 const ip = ["72.14.190.106", "176.58.101.66", "103.3.61.241", "101.207.120.16"];
 const geo = ["US", "UK", "SG", "联通"];
-let index, error, num, indexed, unIndexed, stop, googleTabId, data;
+let googleTabId, data, dataIndex, stop, error, num, indexedNum, unIndexedNum;
 function init() {
   googleTabId = -1;//google搜索页标签id
   data = [];//表格url
-  index = -1;//当前url索引
+  dataIndex = -1;//当前url索引
   stop = true;//搜索是否进行
   error = false;//搜索是否错误
   num = 0;//表格编号
-  indexed = 0;//收录数
-  unIndexed = 0;//未收录数
+  indexedNum = 0;//收录数
+  unIndexedNum = 0;//未收录数
   $("#total").text(num);
+  $("#indexed").text(indexedNum);
+  $("#unIndexed").text(unIndexedNum);
+  updateLocation();
 }
 init();
 //导入数据
@@ -64,6 +67,7 @@ function exportTxt() {
     }, errorHandler);
   });
 
+
 }
 //更新线路信息
 function updateLocation() {
@@ -75,7 +79,7 @@ function updateLocation() {
     }
   });
 }
-updateLocation();
+
 $("body").on('click', '#import', importTxt);
 $("body").on('click', '#export', exportTxt);
 $("body").on('click', '#stop', function () {
@@ -83,15 +87,19 @@ $("body").on('click', '#stop', function () {
   $("#stop").prop({ "id": "check", "value": "Check" });
 });
 $("body").on('click', '#check', function () {
-  if (!data) {
+  if (data.length==0) {
     updateTip('请导入数据');
     return;
   }
-  if (googleTabId == -1) {
+  stop = false;
+  $("#check").prop({ "id": "stop", "value": "Stop" });
+  if (googleTabId == -1) {//第一次搜索
     chrome.tabs.create({ url: googleUrl, active: false, selected: false }, function (tab) {
       googleTabId = tab.id;
       updateTip("正在打开 Google 搜索页");
     });
+  } else {
+    search(dataIndex);
   }
 });
 //更新提示
@@ -101,7 +109,7 @@ function updateTip(tip) {
 //更新搜索结果
 function updateStatus(index, status) {
   //console.log('更新结果：' + index);
-  updateTip("更新结果到表格：" + data[index] + "……");
+  updateTip("更新结果到表格：" + data[index]);
   let statusEle = $("#_" + index).children(".status");
   switch (status) {
     case -2:
@@ -112,12 +120,12 @@ function updateStatus(index, status) {
       break;
     case 0:
       statusEle.removeClass().addClass("status is-danger has-text-centered no").text("NO");
-      unIndexed++;
+      unIndexedNum++;
       $("#unIndexed").text($(".no").length);
       break;
     case 1:
       statusEle.removeClass().addClass("status is-success has-text-centered yes").text("YES");
-      indexed++;
+      indexedNum++;
       $("#indexed").text($(".yes").length);
       break;
   }
@@ -126,7 +134,7 @@ function updateStatus(index, status) {
 function search(index) {
   //console.log('请求搜索：' + index+':'+data[index]);
   chrome.tabs.sendMessage(googleTabId, { q: "site:" + data[index] }, function (response) {
-    updateTip("正在搜索：" + data[index] + "……");
+    updateTip("正在搜索：" + data[index]);
     updateStatus(index, response.status);
   });
 
@@ -134,34 +142,39 @@ function search(index) {
 //获取搜索结果
 function getSearchResult(index) {
   //console.log('请求获取结果：' + index);
-  updateTip("正在获取结果：" + data[index] + "……");
+  updateTip("正在获取结果：" + data[index]);
   chrome.tabs.sendMessage(googleTabId, { g: true }, function (response) {
     //console.log(response);
     updateStatus(index, response.status);
+    //进入下一次搜索
+    dataIndex+=1;
+    if (dataIndex<data.length) {
+      if(!stop){
+        console.log(dataIndex);
+        search(dataIndex);
+      }
+    } else {
+      updateTip('处理完成');
+      stop= true;
+      $("#stop").prop({ "id": "check", "value": "Check" });
+    };
   });
 }
 //监听标签状态
 chrome.tabs.onUpdated.addListener(function (tabId, info, tab) {
-  if (googleTabId != -1 && tabId == googleTabId && info.status == 'complete') {
+  if (googleTabId != -1 && tabId == googleTabId && info.status == 'complete') {//监听Google搜索页标签状态
     let url = new URL(tab.url);
     switch (url.pathname) {
-      case '/sorry':
+      case '/sorry'://验证码页面
         updateTip('搜索失败，请完成验证');
         break;
-      case '/search':
-        //let q = new URLSearchParams(url.search).get('q');
-        getSearchResult(index);
-        index++;
-        if (data[index]) {
-          search(index);
-        } else {
-          updateTip('处理完成');
-        };
+      case '/search'://正常搜索页面
+        getSearchResult(dataIndex);
         break;
       default:
-        if (index == -1) {
-          index += 1;
-          search(index);
+        if (dataIndex == -1) {//开始第一次搜索
+          dataIndex += 1;
+          search(dataIndex);
         }
     }
   }
